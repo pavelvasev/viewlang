463b4253b6a035b8205c4a1a8a755269017c4f99
Pavel Vasev <pavel.vasev@gmail.com>
2016-04-08 18:03:40 +0500

    update threejs before merge webvr things

--- code/setup_vl_assoc_vr.cmd
+++ code/setup_vl_assoc_vr.cmd
@@ -1,4 +1,4 @@
 regedt32 /s cleanreg.reg 
 assoc .vl=VisualLanguangeFile
-ftype VisualLanguangeFile=C:\vasev\Chromium_Win_Oculus_0.8.0.0\chrome.exe --allow-file-access-from-files %~dp0scene.html?s--%%1
+ftype VisualLanguangeFile=C:\vasev\Chrome-bin\chrome.exe --allow-file-access-from-files %~dp0scene.html?s--%%1
 pause
