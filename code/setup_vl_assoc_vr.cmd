a30b8c16881ff0351eeb352a1b385d36cdcf7a7b
Pavel Vasev <pavel.vasev@gmail.com>
2016-04-08 18:40:05 +0500

    правка

--- code/setup_vl_assoc_vr.cmd
+++ code/setup_vl_assoc_vr.cmd
@@ -1,4 +1,5 @@
 regedt32 /s cleanreg.reg 
 assoc .vl=VisualLanguangeFile
-ftype VisualLanguangeFile=C:\vasev\Chrome-bin\chrome.exe --allow-file-access-from-files %~dp0scene.html?s--%%1
+ftype VisualLanguangeFile=C:\vasev\Chrome-bin\chrome.exe --enable-logging --allow-file-access-from-files %~dp0scene.html?s--%%1
+rem --enable-logging --v=1 почемуто последний аргумент вышыбает фтайп винды..
 pause
